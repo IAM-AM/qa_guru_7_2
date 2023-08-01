048ee8e5d2f1f1737e435391c33c09a4085d255c
IAM-AM <aliaksandr.manko1@gmail.com>
2023-08-02 02:48:30 +0400

    Added second test, refactored tests, added exact url into each test

--- tests/test_google_search.py
+++ tests/test_google_search.py
@@ -1,7 +1,22 @@
 from conftest import *
 
 
-def test_find_text():
+def test_find_text_positive_test(opening_browser, set_browser_windows_size):
     browser.open("https://google.com")
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+
+def test_find_text_negative(opening_browser, set_browser_windows_size):
+    browser.open("https://google.com")
+    browser.element('[name="q"]').should(be.blank).type('wqfsdfsfwe').press_enter()
+    browser.element('[id="topstuff"]').should(have.text('Your search - wqfsdfsfwe - did not match any documents.'))
+
+
+
+
+
+
+
+
+
