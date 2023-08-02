df69e19dcdd62c21d517ef561b68d884a888731b
IAM-AM <aliaksandr.manko1@gmail.com>
2023-08-03 02:05:55 +0400

    Removed import unnecessary libraries Removed Opening browser, replace into the fixture

--- tests/test_google_search.py
+++ tests/test_google_search.py
@@ -1,14 +1,12 @@
-from conftest import *
+from selene import be, have, browser
 
 
 def test_find_text_positive_test(opening_browser, set_browser_windows_size):
-    browser.open("https://google.com")
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
 
 def test_find_text_negative(opening_browser, set_browser_windows_size):
-    browser.open("https://google.com")
     browser.element('[name="q"]').should(be.blank).type('wqfsdfsfwe').press_enter()
     browser.element('[id="topstuff"]').should(have.text('Your search - wqfsdfsfwe - did not match any documents.'))
 
