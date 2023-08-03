e557aaa4c3592508d1013a2530522bfeac787790
IAM-AM <aliaksandr.manko1@gmail.com>
2023-08-04 00:58:22 +0400

    Changed fixture parameters Remove unnecessary function (replaced into the test)

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,12 +2,7 @@ import pytest
 from selene import browser
 
 
-@pytest.fixture(scope="session")
-def opening_browser():
-    browser.open("https://google.com")
-
-
-@pytest.fixture()
+@pytest.fixture(scope="function", autouse=True)
 def set_browser_windows_size():
     browser.config.window_height = 2560
     browser.config.window_width = 1440
